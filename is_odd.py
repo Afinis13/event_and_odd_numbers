646fd71a1607920a8bf5d6145252f97557861e15
Afinis13 <afinis13s@gmail.com>
2023-12-10 08:15:02 +0500

    bajarildi

--- is_odd.py
+++ is_odd.py
@@ -1,3 +1,10 @@
 #Create a variable "number" and assign it a integer value.
 
 #Print 1 if the number is odd, otherwise print 0.
+number=type(int)
+number=87
+if (number%2==0):
+   print(0)
+else:
+   print(1)
+
