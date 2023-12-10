331e5dc15fd53aaa0a8708e9ec23a1cf39acd532
Afinis13 <afinis13s@gmail.com>
2023-12-10 08:06:54 +0500

    bajarildi

--- is_even.py
+++ is_even.py
@@ -1,3 +1,9 @@
 #Create a variable "number" and assign it a integer value.
 
 #Print 1 if the number is even, otherwise print 0.
+number=51
+if (number%2==0) :
+    print (0)
+else:
+    print(1)    
+
